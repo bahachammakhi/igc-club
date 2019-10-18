5784351714a26c730bd23ec9b914cfd7c013cb30
hakimmaaouia <hakimmaaouia@gmail.com>
2019-10-18 20:17:00 +0100

    nav1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css';
-import Header from'./componet/layout/header'
 function App() {
   return ( 
     <div className="App">
-        <Header /> 
+  
     </div>
   );
 }
--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import './header.css'
+import './header.css';
+import Logo from './logo.jpg' ;
  const header = () => {
 return (
-    <div className="size">yru</div>
+    <div className="header">
+      <img className="logo" src={Logo}></img>
+     <div className="cn-btn">rezrzerzer</div>
+    </div>
 );
 
  }
