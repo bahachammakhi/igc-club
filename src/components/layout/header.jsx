ec6e4248b992a988b753325f863aeea1813d41bd
hakimmaaouia <hakimmaaouia@gmail.com>
2019-10-18 23:40:30 +0100

    nav2

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import './header.css';
-import Logo from './logo.jpg' ;
- const Header = () => {
-return (
+import React from "react";
+import { Link } from "react-router-dom";
+import "./header.css";
+import Logo from "./logo.jpg";
+import { Icon } from "antd";
+const objnav = [
+  "Home",
+  "Contact",
+  "Notre équipe",
+  "Qui sommes-nous",
+  "Evénements"
+];
+const content = objnav.map(post => (
+  <Link to="first" className="nav-btn" style={{ textDecoration: "none" }}>
+    {post}
+  </Link>
+));
+
+const Header = ({ match }) => {
+  return (
     <div className="header">
       <img className="logo" src={Logo}></img>
-     <div className="cn-btn">rezrzerzer</div>
+      <div className="cn-btn">
+        {content}
+        <div className="media">
+          <a href="http://www.google.com">
+            <Icon type="facebook" className="logo-media" />
+          </a>
+          <a href="http://www.google.com">
+            <Icon type="twitter" className="logo-media" />
+          </a>
+          <a href="http://www.google.com">
+            <Icon type="instagram" className="logo-media" />{" "}
+          </a>
+        </div>
+      </div>
     </div>
-);
-
- }
-export default Header ;
+  );
+};
+export default Header;
