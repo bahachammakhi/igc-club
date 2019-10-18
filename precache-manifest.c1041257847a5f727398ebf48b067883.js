0c50c91b3e67a208d30d5bdde442946ed1811fb9
Baha Chammakhi <bahachammakhi25@gmail.com>
2019-10-19 00:55:14 +0100

    Updates

---
+++ precache-manifest.c1041257847a5f727398ebf48b067883.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48db82a3d564fc327babe2f9cdb3552f",
+    "url": "/igc-club/index.html"
+  },
+  {
+    "revision": "1619028de1c4d46ad39c",
+    "url": "/igc-club/static/css/2.b48b1b29.chunk.css"
+  },
+  {
+    "revision": "a191fa2271bf5532f7be",
+    "url": "/igc-club/static/css/main.e93fff88.chunk.css"
+  },
+  {
+    "revision": "1619028de1c4d46ad39c",
+    "url": "/igc-club/static/js/2.e7758764.chunk.js"
+  },
+  {
+    "revision": "a191fa2271bf5532f7be",
+    "url": "/igc-club/static/js/main.8f997722.chunk.js"
+  },
+  {
+    "revision": "d314e50d490ddb3c340e",
+    "url": "/igc-club/static/js/runtime-main.bd7f445a.js"
+  },
+  {
+    "revision": "31b2fc7068538a98b20f1d37ce04b4d7",
+    "url": "/igc-club/static/media/logo.31b2fc70.jpg"
+  }
+]);
