e65d225e400b4050267360837f51dfce8b167a4b
hakimmaaouia <hakimmaaouia@gmail.com>
2019-10-18 20:26:21 +0100

    change-name

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './header.css';
 import Logo from './logo.jpg' ;
- const header = () => {
+ const Header = () => {
 return (
     <div className="header">
       <img className="logo" src={Logo}></img>
@@ -10,4 +10,4 @@ return (
 );
 
  }
-export default header ;
+export default Header ;
